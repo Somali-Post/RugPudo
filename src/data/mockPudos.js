61191244086c145322f83915b0f694663015f405
Somali-Post <Abdiazizga'al@moct.gov.so>
2025-10-17 19:01:55 +0300

    added more pudo's

--- src/data/mockPudos.js
+++ src/data/mockPudos.js
@@ -1,6 +1,14 @@
 export const MOCK_PUDO_DATA = [
-  { id: '1', name: 'Somali Postal Service', district: 'Boondheere', lat: 2.04027, lng: 45.34715, hours: 'Opens 8 AM', distance: '1.8', rating: 4.8, reviewCount: 150, phone: '+252 61 1112222' },
-  { id: '2', name: 'Hayat Market', district: 'Boondheere', lat: 2.04162, lng: 45.34816, hours: 'Opens 9 AM', distance: '2.1', rating: 4.5, reviewCount: 78, phone: '+252 61 3334444' },
-  { id: '3', name: 'Hayat Market (KM5 Zope)', district: 'Hodan', lat: 2.03084, lng: 45.30356, hours: 'Open 24 hours', distance: '3.5', rating: 4.6, reviewCount: 210, phone: '+252 61 5556666' },
-  { id: '4', name: 'Hayat Market (KM4 Taleex)', district: 'Waaberi', lat: 2.03209, lng: 45.31291, hours: 'Opens 7 AM', distance: '2.9', rating: 4.7, reviewCount: 180, phone: '+252 61 7778888' },
+  // Based on latest list shared
+  { id: '1',  name: 'Somali Postal Service',         district: 'Boondheere',  lat: 2.04027, lng: 45.34715, hours: '08:30 - 16:30', distance: '1.8', rating: 4.9, reviewCount: 1875, phone: '+252 61 1112222' },
+  { id: '2',  name: 'Hayat Market',                   district: 'Boondheere',  lat: 2.04162, lng: 45.34816, hours: '07:00 - 01:00', distance: '2.1', rating: 4.7, reviewCount: 124,  phone: '+252 61 7778888' },
+  { id: '3',  name: 'Cadale Electronics',             district: 'Karaan',      lat: 2.05320, lng: 45.36881, hours: '08:00 - 21:00', distance: '2.7', rating: 4.7, reviewCount: 76,   phone: '+252 61 2222222' },
+  { id: '4',  name: 'Nabil Gas Station Lido',         district: 'Abdiasis',    lat: 2.03740, lng: 45.35812, hours: '07:30 - 23:00', distance: '2.3', rating: 4.6, reviewCount: 95,   phone: '+252 61 2222222' },
+  { id: '5',  name: 'Shirkada Shidaalka HILAAC',      district: 'Karaan',      lat: 2.06518, lng: 45.35426, hours: '06:00 - 22:00', distance: '3.1', rating: 4.5, reviewCount: 60,   phone: '+252 61 2222222' },
+  { id: '6',  name: 'Al Kowther Shopping Centre',     district: 'Yaaqshiid',   lat: 2.08836, lng: 45.34000, hours: '08:30 - 23:59', distance: '4.8', rating: 4.8, reviewCount: 210,  phone: '+252 61 2222222' },
+  { id: '7',  name: 'Feynuso Shop',                   district: 'Wardhigley',  lat: 2.05456, lng: 45.33320, hours: '09:00 - 22:30', distance: '4.4', rating: 4.4, reviewCount: 88,   phone: '+252 61 2222222' },
+  { id: '8',  name: 'Casmir Supermarket',             district: 'Daynile',     lat: 2.07999, lng: 45.31009, hours: '07:00 - 23:59', distance: '4.2', rating: 4.6, reviewCount: 120,  phone: '+252 61 2222222' },
+  { id: '9',  name: 'Madina Supermarket',             district: 'Dharkenley',  lat: 2.03041, lng: 45.27334, hours: '08:00 - 23:00', distance: '6.2', rating: 4.5, reviewCount: 99,   phone: '+252 61 2222222' },
+  { id: '10', name: 'Hayat Market (KM5 Zope)',        district: 'Hodan',       lat: 2.03084, lng: 45.30356, hours: '09:00 - 23:59', distance: '3.5', rating: 4.6, reviewCount: 210,  phone: '+252 61 5556666' },
+  { id: '11', name: 'Hayat Market (KM4 Taleex)',      district: 'Wasberi',     lat: 2.03209, lng: 45.31291, hours: '07:00 - 23:00', distance: '2.9', rating: 4.7, reviewCount: 180,  phone: '+252 61 7778888' },
 ];
